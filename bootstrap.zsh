8f531b4ea036757fde8c9f854ef56926ec6c8127
Jana Radhakrishnan <mrjana@gmail.com>
2025-03-12 15:14:24 -0700

    Enhance bootstrap script to detect CPU architecture and set Homebrew path accordingly

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,8 +1,28 @@
 #!/usr/bin/env zsh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Detect CPU architecture
+ARCH=$(uname -m)
+
+# Set Homebrew path based on architecture
+if [[ "$ARCH" == "arm64" ]]; then
+    BREW_PREFIX="/opt/homebrew"
+else
+    BREW_PREFIX="/usr/local"
+fi
+
+# Ensure Homebrew binary is in PATH
+if [[ -d "$BREW_PREFIX/bin" ]]; then
+    export PATH="$BREW_PREFIX/bin:$PATH"
+    eval "$($BREW_PREFIX/bin/brew shellenv)"
+    echo "Homebrew is set up at $BREW_PREFIX"
+else
+    echo "Homebrew not found at expected location ($BREW_PREFIX)."
+    exit 1
+fi
+
 brew install chezmoi
 brew install --cask git-credential-manager
 mkdir -p ~/.local/share
-git clone https://github.com/mrjana/dotfiles.git ~/.local/share/chezmoi
-chezmoi init --apply
+chezmoi init mrjana --apply
